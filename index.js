a9a5d0ab4e0c48f01ff228ee1b34822bf39cfb43
andrew-coleman <andrew_coleman@uk.ibm.com>
2018-03-26 21:54:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,370 @@
+function numberToWords(value, ordinal) {
+    const few = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const ordinals = ['zeroth', 'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth',
+        'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
+    const decades = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const magnitudes = ['thousand', 'million', 'billion', 'trillion'];
+
+    var lookup = function(num, prev, ord) {
+        var words = '';
+        if (num <= 19) {
+            words = (prev ? ' and ' : '') + (ord ? ordinals[num] : few[num]);
+        } else if (num < 100) {
+            const tens = Math.floor(num / 10);
+            const remainder = num % 10;
+            words = (prev ? ' and ' : '') + decades[tens - 2];
+            if(remainder > 0) {
+                words += '-' + lookup(remainder, false, ord);
+            } else if(ord) {
+                words = words.substring(0, words.length - 1) + 'ieth';
+            }
+        } else if (num < 1000) {
+            const hundreds = Math.floor(num / 100);
+            const remainder = num % 100;
+            words = (prev ? ', ' : '') + few[hundreds] + ' hundred';
+            if(remainder > 0) {
+                words += lookup(remainder, true, ord);
+            } else if(ord) {
+                words += 'th';
+            }
+        } else {
+            const mag = Math.floor(Math.log10(num)/3);
+            const factor = Math.pow(10, mag * 3);
+            const mant = Math.floor(num / factor);
+            const remainder = num - mant * factor;
+            words = (prev ? ', ' : '') + lookup(mant, false, false) + ' ' + magnitudes[mag - 1];
+            if(remainder > 0) {
+                words += lookup(remainder, true, ord);
+            } else if(ord) {
+                words += 'th';
+            }
+        }
+        return words;
+    };
+
+    var words = lookup(value, false, ordinal);
+    return words;
+}
+
+const romanNumerals = [
+    [1000, 'M', 'm'],
+    [900, 'CM', 'cm'],
+    [500, 'D', 'd'],
+    [400, 'CD', 'cd'],
+    [100, 'C', 'c'],
+    [90, 'XC', 'xc'],
+    [50, 'L', 'l'],
+    [40, 'XL', 'xl'],
+    [10, 'X', 'x'],
+    [9, 'IX', 'ix'],
+    [5, 'V', 'v'],
+    [4, 'IV', 'iv'],
+    [1, 'I', 'i']
+];
+
+function numberToRoman(value, uppercase) {
+    for(var index = 0; index < romanNumerals.length; index++) {
+        const numeral = romanNumerals[index];
+        if(value >= numeral[0]) {
+            return numeral[uppercase ? 1 : 2] + numberToRoman(value - numeral[0], uppercase);
+        }
+    }
+    return '';
+}
+
+function formatInteger(value, picture) {
+    if(typeof value === 'undefined') {
+        return undefined;
+    }
+    
+    var formattedInteger;
+
+    const count = function(str, char) {
+        var count = 0;
+        for(var i = 0; i < str.length; i++) {
+            if(str.charAt(i) === char) {
+                count++;
+            }
+        }
+        return count;
+    };
+    
+    var primaryFormat, formatModifier;
+    var ordinal = false;
+    var semicolon = picture.lastIndexOf(';');
+    if(semicolon === -1) {
+        primaryFormat = picture;
+    } else {
+        primaryFormat = picture.substring(0, semicolon);
+        formatModifier = picture.substring(semicolon + 1);
+        if(formatModifier[0] === 'o') {
+            ordinal = true;
+        }
+    }
+    
+    switch(picture[0]) {
+        case 'A':
+        case 'a':
+            break;
+        case 'i':
+        case 'I':
+            formattedInteger = numberToRoman(value, picture[0] === 'I');
+            break;
+        case 'w':
+            formattedInteger = numberToWords(value, ordinal);
+            break;
+        case 'W': // TODO check for Ww
+            formattedInteger = numberToWords(value, ordinal).toUpperCase();
+            break;
+        default:
+            // decimal-digit-pattern
+            const optionalDigits = count(primaryFormat, '#');
+            const groupingSeparators = count(primaryFormat, ',');
+            const mandatoryDigits = primaryFormat.length - optionalDigits - groupingSeparators;
+
+            formattedInteger = '' + value;
+
+            // TODO use functionPad
+            var padLength = mandatoryDigits - formattedInteger.length;
+            if (padLength > 0) {
+                var padding = (new Array(padLength + 1)).join('0');
+                formattedInteger = padding + formattedInteger;
+            }
+            
+            if(ordinal) {
+                var suffix123 = {'1': 'st', '2': 'nd', '3': 'rd'};
+                var lastDigit = formattedInteger[formattedInteger.length - 1];
+                var suffix = suffix123[lastDigit];
+                if(!suffix || (formattedInteger.length > 1 && formattedInteger[formattedInteger.length - 2] === '1')) {
+                    suffix = 'th';
+                }
+                formattedInteger = formattedInteger + suffix;
+            }
+    }
+
+    return formattedInteger;
+}
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function formatDateTime(millis, picture) {
+    if(typeof millis === 'undefined') {
+        return undefined;
+    }
+
+    // the format specifier is an array of string literals and variable markers
+    var parsePicture = function(pic) {
+        var spec = [];
+        var start = 0, pos = 0;
+        while(pos < pic.length) {
+            if(pic.charAt(pos) === '[') { //TODO check it's not a doubled [[
+                // start of variable marker
+                // push the string literal (if there is one) onto the array
+                if(pos > start) {
+                    spec.push({ type: 'literal', value: pic.substring(start, pos) });
+                }
+                start = pos;
+                // search forward to closing ]
+                pos = pic.indexOf(']', start);
+                // TODO handle error case if pos === -1
+                const marker = pic.substring(start+1, pos);
+                var def = {
+                    type: 'marker',
+                    component: marker.charAt(0)
+                };
+                var comma = marker.lastIndexOf(',');
+                var presMod;
+                if(comma !== -1) {
+                    // width modifier
+                    var widthDef;
+                    const widthMod = marker.substring(comma+1);
+                    const dash = widthMod.indexOf('-');
+                    widthDef = {};
+                    if(dash === -1) {
+                        if(widthMod !== '*') {
+                            widthDef.min = parseInt(widthMod)
+                        }
+                    } else {
+                        var min = widthMod.substring(0, dash);
+                        var max = widthMod.substring(dash + 1);
+                        if(min !== '*') {
+                            widthDef.min = parseInt(min);
+                        }
+                        if(max !== '*') {
+                            widthDef.max = parseInt(max);
+                        }
+                    }
+                    def.width = widthDef;
+                    presMod = marker.substring(1, comma);
+                } else {
+                    presMod = marker.substring(1);
+                }
+                if(presMod.length === 1) {
+                    def.presentation1 = presMod;
+                } else if(presMod.length > 1) {
+                    var lastChar = presMod.charAt(presMod.length - 1);
+                    if('atco'.indexOf(lastChar) !== -1) {
+                        def.presentation2 = lastChar;
+                        def.presentation1 = presMod.substring(0, presMod.length - 1);
+                    } else {
+                        def.presentation1 = presMod;
+                    }
+                }
+                spec.push(def);
+                start = pos + 1;
+            }
+            pos++;
+        }
+        if(pos > start) {
+            // last bit of literal text
+            spec.push({ type: 'literal', value: pic.substring(start, pos) });
+        }
+        return spec;
+    };
+
+    String.prototype.replaceAt = function(index, replacement) {
+        return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+    };
+
+    var formatComponent = function(date, markerSpec) {
+        var componentValue;
+        switch (markerSpec.component) {
+            case 'Y':
+                componentValue = date.getFullYear();
+                break;
+            case 'M':
+                componentValue = date.getMonth() + 1;
+                break;
+            case 'D':
+                componentValue = date.getDate();
+                break;
+            case 'd':
+                componentValue = date.getDate();  // TODO day of the year
+                break;
+            case 'F':
+                componentValue = date.getDay();
+                break;
+            case 'W':
+                componentValue = date.getDate();  // TODO week of the year
+                break;
+            case 'w':
+                componentValue = date.getDate();  // TODO week of the month
+                break;
+            case 'H':
+                componentValue = date.getHours();
+                break;
+            case 'h':
+                componentValue = date.getHours();  // TODO 12 hour format
+                break;
+            case 'P':
+                componentValue = date.getHours() >= 12 ? 'pm' : 'am';
+                break;
+            case 'm':
+                componentValue = date.getMinutes();
+                break;
+            case 's':
+                componentValue = date.getSeconds();
+                break;
+            case 'f':
+                componentValue = date.getMilliseconds();
+                break;
+            case 'Z':
+                componentValue = date.getDate();  // TODO
+                break;
+            case 'z':
+                componentValue = date.getDate();// TODO
+                break;
+            case 'C':
+                componentValue = date.getDate();// TODO
+                break;
+            case 'E':
+                componentValue = date.getDate();// TODO
+                break;
+        }
+        // §9.8.4.3
+        if('YMDdFWwHhms'.indexOf(markerSpec.component) !== -1) {
+            if(markerSpec.component === 'Y') {
+                // §9.8.4.4
+                var n = -1;
+                if(markerSpec.width && markerSpec.width.min !== undefined) {
+                    n = markerSpec.width.min;
+                } else if(markerSpec.presentation1) {
+                    var w = markerSpec.presentation1;  // TODO only count the optional and mandatory digit signs
+                    if(w >= 2) {
+                        n = w;
+                    }
+                }
+                if(n !== -1) {
+                    componentValue = componentValue % Math.pow(10, n);
+                }
+            }
+            if(markerSpec.presentation1 === 'Nn' || markerSpec.presentation1 === 'N' || markerSpec.presentation1 === 'n') {
+                if(markerSpec.component === 'M') {
+                    componentValue = months[componentValue - 1];
+                } else if(markerSpec.component === 'F') {
+                    componentValue = days[componentValue - 1];
+                }
+                if(markerSpec.presentation1 === 'N') {
+                    componentValue = componentValue.toUpperCase();
+                } else if(markerSpec.presentation1 === 'n') {
+                    componentValue = componentValue.toLowerCase();
+                }
+                if(markerSpec.width && componentValue.length > markerSpec.width.max) {
+                    componentValue = componentValue.substring(0, markerSpec.width.max);
+                }
+            } else {
+                var integerPattern = markerSpec.presentation1;
+                if (typeof integerPattern === 'undefined') {
+                    integerPattern = '0';
+                }
+                if (markerSpec.width && markerSpec.width.min !== undefined) {
+                    var specifiedWidth = 0;
+                    var pos = integerPattern.length - 1;
+                    while (specifiedWidth < markerSpec.width.min) {
+                        if (pos < 0) {
+                            integerPattern = '0' + integerPattern;
+                            specifiedWidth++;
+                            continue;
+                        }
+                        var currentChar = integerPattern.charAt(pos);
+                        if ('0123456789'.indexOf(currentChar) !== -1) {
+                            specifiedWidth++;
+                            pos--;
+                        } else if (currentChar === '#') {
+                            integerPattern = integerPattern.replaceAt(pos, '0');
+                            specifiedWidth++;
+                            pos--
+                        }
+                    }
+                }
+                if (markerSpec.presentation2) {
+                    integerPattern += ';' + markerSpec.presentation2;
+                }
+                componentValue = formatInteger(componentValue, integerPattern);
+            }
+        }
+        return componentValue;
+    };
+
+    var formatSpec =  parsePicture(picture);
+
+    var dateTime = new Date(millis);
+
+    var result = '';
+    formatSpec.forEach(function(part) {
+        if(part.type === 'literal') {
+            result += part.value;
+        } else {
+            result += formatComponent(dateTime, part);
+        }
+    });
+
+    return result;
+}
+
+module.exports = {
+    formatInteger: formatInteger,
+    formatDateTime: formatDateTime
+};
---
+++ test/test.js
@@ -0,0 +1,381 @@
+
+
+var formatInteger = require('../index').formatInteger;
+var formatDateTime = require('../index').formatDateTime;
+var assert = require('assert');
+var chai = require("chai");
+var expect = chai.expect;
+
+
+describe('#formatInteger', function () {
+    describe('undefined value', function () {
+        it('should return result object', function () {
+            var result = formatInteger(undefined);
+            var expected = undefined;
+            assert.equal(result, expected);
+        });
+    });
+
+    describe('decimal-digit-pattern - cardinal', function () {
+        it('should format a number', function () {
+            var result = formatInteger(123, '000');
+            var expected = '123';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(123, '0000');
+            var expected = '0123';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(1234, '0');
+            var expected = '1234';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(1234, '###0');
+            var expected = '1234';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(12, '###0');
+            var expected = '12';
+            assert.equal(result, expected);
+        });
+
+    });
+
+    describe('decimal-digit-pattern - ordinal', function () {
+        it('should format a number', function () {
+            var result = formatInteger(123, '000;o');
+            var expected = '123rd';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(1, '0;o');
+            var expected = '1st';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(28, '#0;o');
+            var expected = '28th';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(1234, '###0;o');
+            var expected = '1234th';
+            assert.equal(result, expected);
+        });
+
+        it('should pad number with zeros', function () {
+            var result = formatInteger(12, '###0;o');
+            var expected = '12th';
+            assert.equal(result, expected);
+        });
+
+    });
+
+    describe('roman numerals', function () {
+        it('0', function () {
+            var result = formatInteger(0, 'I');
+            var expected = '';
+            assert.equal(result, expected);
+        });
+
+        it('1984', function () {
+            var result = formatInteger(1984, 'I');
+            var expected = 'MCMLXXXIV';
+            assert.equal(result, expected);
+        });
+
+        it('99', function () {
+            var result = formatInteger(99, 'i');
+            var expected = 'xcix';
+            assert.equal(result, expected);
+        });
+
+    });
+
+    describe('words - cardinal', function () {
+        it('12', function () {
+            var result = formatInteger(12, 'w');
+            var expected = 'twelve';
+            assert.equal(result, expected);
+        });
+
+        it('20', function () {
+            var result = formatInteger(20, 'w');
+            var expected = 'twenty';
+            assert.equal(result, expected);
+        });
+
+        it('34', function () {
+            var result = formatInteger(34, 'w');
+            var expected = 'thirty-four';
+            assert.equal(result, expected);
+        });
+
+        it('99', function () {
+            var result = formatInteger(99, 'W');
+            var expected = 'NINETY-NINE';
+            assert.equal(result, expected);
+        });
+
+        it('100', function () {
+            var result = formatInteger(100, 'w');
+            var expected = 'one hundred';
+            assert.equal(result, expected);
+        });
+
+        it('555', function () {
+            var result = formatInteger(555, 'W');
+            var expected = 'FIVE HUNDRED AND FIFTY-FIVE';
+            assert.equal(result, expected);
+        });
+
+        it('919', function () {
+            var result = formatInteger(919, 'w');
+            var expected = 'nine hundred and nineteen';
+            assert.equal(result, expected);
+        });
+
+        it('730', function () {
+            var result = formatInteger(730, 'w');
+            var expected = 'seven hundred and thirty';
+            assert.equal(result, expected);
+        });
+
+        it('1000', function () {
+            var result = formatInteger(1000, 'w');
+            var expected = 'one thousand';
+            assert.equal(result, expected);
+        });
+
+        it('3730', function () {
+            var result = formatInteger(3730, 'w');
+            var expected = 'three thousand, seven hundred and thirty';
+            assert.equal(result, expected);
+        });
+
+        it('327730', function () {
+            var result = formatInteger(327730, 'w');
+            var expected = 'three hundred and twenty-seven thousand, seven hundred and thirty';
+            assert.equal(result, expected);
+        });
+
+        it('4327730', function () {
+            var result = formatInteger(4327730, 'w');
+            var expected = 'four million, three hundred and twenty-seven thousand, seven hundred and thirty';
+            assert.equal(result, expected);
+        });
+
+        it('1e12 + 1', function () {
+            var result = formatInteger(1e12 + 1, 'w');
+            var expected = 'one trillion and one';
+            assert.equal(result, expected);
+        });
+
+    })
+
+    describe('words - ordinal', function () {
+        it('12', function () {
+            var result = formatInteger(12, 'w;o');
+            var expected = 'twelfth';
+            assert.equal(result, expected);
+        });
+
+        it('20', function () {
+            var result = formatInteger(20, 'w;o');
+            var expected = 'twentieth';
+            assert.equal(result, expected);
+        });
+
+        it('34', function () {
+            var result = formatInteger(34, 'w;o');
+            var expected = 'thirty-fourth';
+            assert.equal(result, expected);
+        });
+
+        it('99', function () {
+            var result = formatInteger(99, 'W;o');
+            var expected = 'NINETY-NINTH';
+            assert.equal(result, expected);
+        });
+
+        it('100', function () {
+            var result = formatInteger(100, 'w;o');
+            var expected = 'one hundredth';
+            assert.equal(result, expected);
+        });
+
+        it('555', function () {
+            var result = formatInteger(555, 'W;o');
+            var expected = 'FIVE HUNDRED AND FIFTY-FIFTH';
+            assert.equal(result, expected);
+        });
+
+        it('919', function () {
+            var result = formatInteger(919, 'w;o');
+            var expected = 'nine hundred and nineteenth';
+            assert.equal(result, expected);
+        });
+
+        it('730', function () {
+            var result = formatInteger(730, 'w;o');
+            var expected = 'seven hundred and thirtieth';
+            assert.equal(result, expected);
+        });
+
+        it('1000', function () {
+            var result = formatInteger(1000, 'w;o');
+            var expected = 'one thousandth';
+            assert.equal(result, expected);
+        });
+
+        it('3730', function () {
+            var result = formatInteger(3731, 'w;o');
+            var expected = 'three thousand, seven hundred and thirty-first';
+            assert.equal(result, expected);
+        });
+
+        it('327730', function () {
+            var result = formatInteger(327713, 'w;o');
+            var expected = 'three hundred and twenty-seven thousand, seven hundred and thirteenth';
+            assert.equal(result, expected);
+        });
+
+        it('4327730', function () {
+            var result = formatInteger(4327732, 'w;o');
+            var expected = 'four million, three hundred and twenty-seven thousand, seven hundred and thirty-second';
+            assert.equal(result, expected);
+        });
+
+        it('1e12 + 1', function () {
+            var result = formatInteger(1e12 + 1, 'w;o');
+            var expected = 'one trillion and first';
+            assert.equal(result, expected);
+        });
+
+    })
+});
+
+describe('#formatDateTime', function () {
+    describe('undefined value', function () {
+        it('should return result object', function () {
+            var result = formatDateTime(undefined);
+            var expected = undefined;
+            assert.equal(result, expected);
+        });
+    });
+
+    describe('basic date patterns', function () {
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, 'Hello');
+            var expected = 'Hello';
+            assert.equal(result, expected);
+        });
+
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, 'Year: [Y0001]');
+            var expected = 'Year: 2018';
+            assert.equal(result, expected);
+        });
+
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, 'Year: <[Y0001]>');
+            var expected = 'Year: <2018>';
+            assert.equal(result, expected);
+        });
+
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, '[D#1]/[M#1]/[Y0001]');
+            var expected = '23/3/2018';
+            assert.equal(result, expected);
+        });
+
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, '[Y0001]-[M01]-[D01]');
+            var expected = '2018-03-23';
+            assert.equal(result, expected);
+        });
+
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, '[M01]/[D01]/[Y0001] at [H01]:[m01]:[s01]');
+            var expected = '03/23/2018 at 10:33:36';
+            assert.equal(result, expected);
+        });
+
+    });
+
+    describe('width modifier', function () {
+        it('should return literal', function () {
+            var result = formatDateTime(1521801216617, '[D#1,2]/[M1,2]/[Y,2]');
+            var expected = '23/03/18';
+            assert.equal(result, expected);
+        });
+
+    });
+
+    describe('roman numeral dates', function () {
+        it('year in roman numerals', function () {
+            var result = formatDateTime(1521801216617, '[D1] [M01] [YI]');
+            var expected = '23 03 MMXVIII';
+            assert.equal(result, expected);
+        });
+    });
+
+    describe('ordinals', function () {
+        it('day in ordinal numerals', function () {
+            var result = formatDateTime(1521801216617, '[D1o] [M01] [Y]');
+            var expected = '23rd 03 2018';
+            assert.equal(result, expected);
+        });
+    });
+
+    describe('date in words', function () {
+        it('year in words', function () {
+            var result = formatDateTime(1521801216617, '[Yw]');
+            var expected = 'two thousand and eighteen';
+            assert.equal(result, expected);
+        });
+
+        it('day of month in ordinal words', function () {
+            var result = formatDateTime(1521801216617, '[Dwo] [M01] [Y]');
+            var expected = 'twenty-third 03 2018';
+            assert.equal(result, expected);
+        });
+
+        it('month in words', function () {
+            var result = formatDateTime(1521801216617, '[D1o] [MNn] [Y]');
+            var expected = '23rd March 2018';
+            assert.equal(result, expected);
+        });
+
+        it('month in uppercase words', function () {
+            var result = formatDateTime(1521801216617, '[D1o] [MN] [Y]');
+            var expected = '23rd MARCH 2018';
+            assert.equal(result, expected);
+        });
+
+        it('day/month in words', function () {
+            var result = formatDateTime(1521801216617, '[FNn], [D1o] [MNn] [Y]');
+            var expected = 'Thursday, 23rd March 2018';
+            assert.equal(result, expected);
+        });
+
+        it('abbreviated day/month in words', function () {
+            var result = formatDateTime(1521801216617, '[FNn,3-3], [D1o] [MNn,3-3] [Y]');
+            var expected = 'Thu, 23rd Mar 2018';
+            assert.equal(result, expected);
+        });
+
+    });
+
+});
